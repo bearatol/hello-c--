ec51b6d42f0211ce3b559228763e76ba92c99738
Igor Khristenko <ikhristenko@avaya.com>
2021-07-16 14:57:11 +0300

    fup

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    cout << "Hello world!" << endl;
+
+    char ch = 'a';
+    string str = "some string";
+    float a = 2.3, b = 3.2;
+
+    cout << ch << endl;
+    cout << str << endl;
+    cout << a + b << endl;
+
+    return 0;
+}
